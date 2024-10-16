0eacbc86443fb54fbbf9fda22d55580801e1ec54
amaksimmmm <ma1935711@gmail.com>
2024-10-16 12:04:48 +0300

    implement  TS

---
+++ js/widgets/Header/types.ts
@@ -0,0 +1,7 @@
+import { LangData } from '../../features/Lang/types';
+import { NavIteamData } from '../../features/Navigation/types';
+
+export type HeaderData = {
+  langs: LangData[];
+  navigation: NavIteamData[];
+};
