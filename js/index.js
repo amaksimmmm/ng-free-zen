2e7fe38aa588fece9a03760c022882eb3be8de27
amaksimmmm <ma1935711@gmail.com>
2024-10-17 00:40:43 +0300

    modify js

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ import { Header } from './widgets/Header/index.js';
 import { Download } from './widgets/Download/index.js';
 import { Warranty } from './widgets/Warranty/index.js';
 import { Care } from './widgets/Care/index.js';
+import { Cashback } from './widgets/Cashback/index.js';
 
 const $root = document.querySelector('#root');
 
@@ -14,6 +15,7 @@ $root?.insertAdjacentHTML('beforeend', Header(data.primaryInfo));
 $root?.insertAdjacentHTML('beforeend', Download(data.download));
 $root?.insertAdjacentHTML('beforeend', Warranty(data.warranty));
 $root?.insertAdjacentHTML('beforeend', Care(data.care));
+$root?.insertAdjacentHTML('beforeend', Cashback(data.cashback));
 
 const $logo = document.querySelector('#logo');
 
