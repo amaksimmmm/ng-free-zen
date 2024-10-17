4e3a9fbf791f724e2951531b00e018761a92b17c
amaksimmmm <ma1935711@gmail.com>
2024-10-17 13:46:29 +0300

    refactored js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 import { data } from './data.js';
-// import { Header } from './widgets/Header/index.js';
-// import { Download } from './widgets/Download/index.js';
-// import { Warranty } from './widgets/Warranty/index.js';
-// import { Care } from './widgets/Care/index.js';
+import { Header } from './widgets/Header/index.js';
+import { Download } from './widgets/Download/index.js';
+import { Warranty } from './widgets/Warranty/index.js';
+import { Care } from './widgets/Care/index.js';
 import { Cashback } from './widgets/Cashback/index.js';
-import { Clients } from './widgets/Clients/Clients.js';
+import { Clients } from './widgets/Clients/index.js';
 
 const $root = document.querySelector('#root');
 
@@ -12,10 +12,10 @@ const $root = document.querySelector('#root');
 //   $root.insertAdjacentHTML('beforeend', Download(data.download));
 // };
 
-// $root?.insertAdjacentHTML('beforeend', Header(data.primaryInfo));
-// $root?.insertAdjacentHTML('beforeend', Download(data.download));
-// $root?.insertAdjacentHTML('beforeend', Warranty(data.warranty));
-// $root?.insertAdjacentHTML('beforeend', Care(data.care));
+$root?.insertAdjacentHTML('beforeend', Header(data.primaryInfo));
+$root?.insertAdjacentHTML('beforeend', Download(data.download));
+$root?.insertAdjacentHTML('beforeend', Warranty(data.warranty));
+$root?.insertAdjacentHTML('beforeend', Care(data.care));
 $root?.insertAdjacentHTML('beforeend', Cashback(data.cashback));
 $root?.insertAdjacentHTML('beforeend', Clients(data.clients));
 
